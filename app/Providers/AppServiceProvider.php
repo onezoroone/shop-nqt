ab01070c8c3fb8398ea2d33684aaed0a99e542e6
onezoroone <xuxuthoi01@gmail.com>
2026-05-01 15:08:50 +0700

    fix ssl for cf

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (request()->header('x-forwarded-proto') == 'https') {
+            \URL::forceScheme('https');
+        }
         Model::preventLazyLoading(! app()->isProduction());
     }
 }
